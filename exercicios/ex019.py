d9749e56be902c08326c18a48394a117c689e531
LeonardoAranha <114034983+LeonardoAranha@users.noreply.github.com>
2022-10-10 19:47:46 -0300

    correção do ex019

--- exercicios/ex019.py
+++ exercicios/ex019.py
@@ -1,9 +1,9 @@
 import random
 print('sorteio da sala 8b')
-n1 = str(input('primeiro aluno: ')).title
-n2 = str(input('segundo aluno: ')).title
-n3 = str(input('terceiro aluno: ')).title
-n4 = str(input('quarto aluno: ')).title
+n1 = str(input('primeiro aluno: ')).title()
+n2 = str(input('segundo aluno: ')).title()
+n3 = str(input('terceiro aluno: ')).title()
+n4 = str(input('quarto aluno: ')).title()
 lista = [n1, n2, n3, n4]
 escolhido = random.choice(lista)
 print(f'o aluno escolhido foi {escolhido}')
